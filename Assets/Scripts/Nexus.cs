32ed8cd89a99ec3d692f4e180a7a0aad69681510
David Maidenberg <david.maidenberg@epitech.eu>
2019-02-23 20:49:42 -0800

    Create Repository Add Basic scene for testing purposes Create IAttackable interaface, and Creep & Nexus class that implement it Create a SpawnController and the -L- bind to test it

---
+++ Assets/Scripts/Nexus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nexus : MonoBehaviour, IAttackable
+{
+    public Vector3 GetPosition() { return this.transform.position; }
+
+    public void ReceiveDamage(int dmg)
+    {
+        //TODO
+        Debug.Log("Receive " + dmg.ToString() + " damage");
+    }
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public static SpawnController instance = null;
+    public GameObject enemyNexus;
+    public GameObject spawnPoint;
+    public GameObject spawnPlateform;
+    public int playerId;
+
+    public List<Creep> creeps = new List<Creep>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void CreateCreep(Creep creep)
+    {
+        creeps.Add(creep);
+    }
+
+    private void SpawnCreeps()
+    {
+        Creep tmp;
+        foreach(Creep creep in creeps)
+        {
+            tmp = Instantiate(creep, spawnPoint.transform);
+            tmp.Init(spawnPoint.transform.position + (creep.transform.position - spawnPlateform.transform.position), enemyNexus.transform.position, playerId);
+            tmp.SetDestination(enemyNexus.transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            SpawnCreeps();
+    }
+}
