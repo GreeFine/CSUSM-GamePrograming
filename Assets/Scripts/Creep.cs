7beacf29c50371c63931a309d06950c5b210ac45
Raphaeël <raphou007@hotmail.fr>
2019-03-02 11:43:48 -0800

    Networking init

--- Assets/Scripts/Creep.cs
+++ Assets/Scripts/Creep.cs
@@ -5,120 +5,121 @@ using UnityEngine.AI;
 
 public class Creep : MonoBehaviour, IAttackable
 {
-    public bool hasAnimator = false;
-    public int maxLife;
-    public int atkDmg;
-    public float atkRange;
-    public float atkSpeed;
+  public bool hasAnimator = false;
+  public int maxLife;
+  public int atkDmg;
+  public float atkRange;
+  public float atkSpeed;
 
-    private int currentLife;
-    private float atkReload = 0f;
-    private Vector3 nexus;
-    private List<IAttackable> targets;
-    private IAttackable currentTarget = null;
-    private bool isBuilding = true;
+  private int currentLife;
+  private float atkReload = 0f;
+  private Vector3 enemy_nexus;
+  private List<IAttackable> targets = new List<IAttackable>();
+  private IAttackable currentTarget = null;
+  private bool isBuilding = true; //FIXME:
+  private uint owner = checked(0);
 
-    public void Init(Vector3 newPos, Vector3 dest, int playerId)
+  private void Awake()
+  {
+    NavMeshAgent agent = this.gameObject.AddComponent<NavMeshAgent>();
+    agent.SetDestination(enemy_nexus);
+    ChangeAnimation("Run");
+  }
+
+  public void Init(uint p_owner, Vector3 p_start_pos, Vector3 p_enemy_nexus)
+  {
+    isBuilding = false;
+    owner = p_owner;
+    this.gameObject.layer = 9 + (int)owner;
+    this.transform.position = p_start_pos;
+    enemy_nexus = p_enemy_nexus;
+    currentLife = maxLife;
+  }
+
+  public void SetDestination(Vector3 dest)
+  {
+    GetComponent<NavMeshAgent>().SetDestination(dest);
+  }
+
+  private void ChooseTarget()
+  {
+    int count = targets.Count;
+    int i = 0;
+    while (i < count && targets[i] == null)
     {
-        isBuilding = false;
-        this.transform.position = newPos;
-        this.gameObject.layer =  8 + playerId;
-
-        NavMeshAgent agent;
-        agent = this.gameObject.AddComponent<NavMeshAgent>();
-        agent.SetDestination(dest);
-        nexus = dest;
-
-        currentLife = maxLife;
-        targets = new List<IAttackable>();
-        ChangeAnimation("Run");
+      targets.RemoveAt(i);
+      i++;
     }
+    targets.Sort(SortByDistance);
+    currentTarget = targets[0];
+  }
 
-    public void SetDestination(Vector3 dest)
+  private void Attack()
+  {
+    if (currentTarget == null)
     {
-        GetComponent<NavMeshAgent>().SetDestination(dest);
+      ChangeAnimation("Run");
+      GetComponent<NavMeshAgent>().destination = enemy_nexus;
+      GetComponent<NavMeshAgent>().isStopped = false;
+      return;
     }
-
-    private void ChooseTarget()
+    if (Vector3.Distance(this.transform.position, currentTarget.GetPosition()) > atkRange)
     {
-        int count = targets.Count;
-        int i = 0;
-        while (i < count && targets[i] == null)
-        {
-            targets.RemoveAt(i);
-            i++;
-        }
-        targets.Sort(SortByDistance);
-        currentTarget = targets[0];
+      GetComponent<NavMeshAgent>().destination = currentTarget.GetPosition();
+      return;
     }
+    GetComponent<NavMeshAgent>().isStopped = true;
+    ChangeAnimation("Attack");
+    atkReload = atkSpeed;
+    currentTarget.ReceiveDamage(atkDmg); //Should have a warm-up time
+  }
 
-    private void Attack()
+  public void Update()
+  {
+    if (isBuilding)
+      return;
+    atkReload -= Time.deltaTime;
+    if (atkReload <= 0)
     {
-        if (currentTarget == null)
-        {
-            ChangeAnimation("Run");
-            GetComponent<NavMeshAgent>().destination = nexus;
-            GetComponent<NavMeshAgent>().isStopped = false;
-            return;
-        }
-        if (Vector3.Distance(this.transform.position, currentTarget.GetPosition()) > atkRange)
-        {
-            GetComponent<NavMeshAgent>().destination = currentTarget.GetPosition();
-            return;
-        }
-        GetComponent<NavMeshAgent>().isStopped = true;
-        ChangeAnimation("Attack");
-        atkReload = atkSpeed;
-        currentTarget.ReceiveDamage(atkDmg);//Should have a warm-up time
+      if (currentTarget == null && targets.Count > 0)
+        ChooseTarget();
+      Attack();
     }
+  }
 
-    public void Update()
-    {
-        if (isBuilding)
-            return;
-        atkReload -= Time.deltaTime;
-        if (atkReload <= 0)
-        {
-            if (currentTarget == null && targets.Count > 0)
-                ChooseTarget();
-            Attack();
-        }
-    }
+  private void ChangeAnimation(string name)
+  {
+    if (hasAnimator)
+      GetComponent<Animator>().SetBool(name, true);
+    else
+      GetComponent<Animation>().Play(name);
+  }
 
-    private void ChangeAnimation(string name)
-    {
-        if (hasAnimator)
-            GetComponent<Animator>().SetBool(name, true);
-        else
-            GetComponent<Animation>().Play(name);
-    }
+  private void OnTriggerEnter(Collider other)
+  {
+    IAttackable tmp = other.GetComponent<IAttackable>();
+    if (tmp != null)
+      targets.Add(tmp);
+  }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        IAttackable tmp = other.GetComponent<IAttackable>();
-        if (tmp != null)
-            targets.Add(tmp);
-    }
+  private void OnTriggerExit(Collider other)
+  {
+    IAttackable tmp = other.GetComponent<IAttackable>();
+    if (tmp != null)
+      targets.Remove(tmp);
+  }
 
-    private void OnTriggerExit(Collider other)
-    {
-        IAttackable tmp = other.GetComponent<IAttackable>();
-        if (tmp != null)
-            targets.Remove(tmp);
-    }
+  private int SortByDistance(IAttackable a, IAttackable b)
+  {
+    return ((this.transform.position - a.GetPosition()).sqrMagnitude.CompareTo((this.transform.position - b.GetPosition()).sqrMagnitude));
+  }
 
-    private int SortByDistance(IAttackable a, IAttackable b)
-    {
-        return ((this.transform.position - a.GetPosition()).sqrMagnitude.CompareTo((this.transform.position - b.GetPosition()).sqrMagnitude));
-    }
-
-
-    //Interface : IAttackable
-    public Vector3 GetPosition() { return this.transform.position; }
-    public void ReceiveDamage(int damage)
-    {
-        currentLife -= damage;
-        if (currentLife <= 0)
-            Destroy(this.gameObject);
-    }
+  //Interface : IAttackable
+  public Vector3 GetPosition() { return this.transform.position; }
+  public void ReceiveDamage(int damage)
+  {
+    currentLife -= damage;
+    if (currentLife <= 0)
+      Destroy(this.gameObject);
+  }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : MonoBehaviour
+{
+  uint id = 0;
+  private Spawner spawner = null;
+
+  private void Awake()
+  {
+    id = GetComponent<NetworkIdentity>().netId.Value;
+    Spawner[] spawners = GameObject.FindObjectsOfType<Spawner>();
+    spawner = spawners[id];
+    Debug.Log("Player spawned:" + id);
+  }
+
+  private void AddCreep()
+  {
+    spawner.CmdNewCreep(id, "test");
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.L))
+      AddCreep();
+    if (Input.GetKeyDown(KeyCode.Escape))
+      Application.Quit();
+  }
+}
