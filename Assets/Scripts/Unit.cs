5edc741b402653679b8cfae3ffc271f5225aed17
David Maidenberg <david.maidenberg@epitech.eu>
2019-03-03 00:35:00 -0800

    Add Healtbar and their logic handle in Unit.Cs Reforge animation handle on attacks

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,26 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.AI;
-
-
+using UnityEngine.UI;
 
 public class Unit : NetworkBehaviour, IAttackable
 {
+    //TODO : struct to organise variables
     public bool hasAnimator = false;
+    public Image healthBar;
     public int maxLife;
     public int atkDmg;
     public float atkRange;
     public float atkSpeed;
-    public float atkWindUp;
-    
+    public float animDmgTime;
+
+    private int pidOwner = -9;
     private int currentLife;
     private float atkReload = 0f;
+    private float modifierAS = 1f; //TODO : implem AS buff/debuff setter
+    private float modifierMS = 1f; //TODO : implem unit speed + animation
+    private bool isAttacking = false;
     private Vector3 enemyNexus;
     private List<GameObject> targets = new List<GameObject>();
     private GameObject currentTarget = null;
     private bool isDead = false;
-    private int pidOwner = -9;
-
+    
 
     private void Start()
     {
@@ -39,6 +43,11 @@ public class Unit : NetworkBehaviour, IAttackable
         this.transform.position = pStartPos;
         enemyNexus = pEnemyNexus;
         currentLife = maxLife;
+        if (pPidOwner == 1)
+        {
+            healthBar.GetComponentInParent<Canvas>().GetComponent<RectTransform>().Rotate(new Vector3(1, 0, 0), -90);
+            healthBar.fillOrigin = 0;
+        }
     }
 
     public void SetDestination(Vector3 dest)
@@ -51,6 +60,8 @@ public class Unit : NetworkBehaviour, IAttackable
         atkReload -= Time.deltaTime;
         if (atkReload <= 0)
         {
+            if (isAttacking)
+                ChangeAnimation("Idle");
             if (currentTarget == null && targets.Count > 0)
                 ChooseTarget();
             Attack();
@@ -85,6 +96,7 @@ public class Unit : NetworkBehaviour, IAttackable
     {
         if (currentTarget == null)
         {
+            isAttacking = false;
             ChangeAnimation("Run");
             GetComponent<NavMeshAgent>().destination = enemyNexus;
             GetComponent<NavMeshAgent>().isStopped = false;
@@ -92,13 +104,15 @@ public class Unit : NetworkBehaviour, IAttackable
         }
         if (Vector3.Distance(this.transform.position, currentTarget.GetComponent<IAttackable>().GetPosition()) > atkRange)
         {
+            isAttacking = false;
             GetComponent<NavMeshAgent>().destination = currentTarget.GetComponent<IAttackable>().GetPosition();
             return;
         }
+        isAttacking = true;
         GetComponent<NavMeshAgent>().isStopped = true;
-        atkReload = atkSpeed;
-        ChangeAnimation("Attack");
-        StartCoroutine(AtkWindUpComplete(atkWindUp, currentTarget));
+        atkReload = atkSpeed * modifierAS;
+        ChangeAnimation("Attack", atkSpeed * modifierAS);
+        StartCoroutine(AtkWindUpComplete(animDmgTime * modifierAS, currentTarget));
     }
 
     private IEnumerator AtkWindUpComplete(float time, GameObject target)
@@ -106,6 +120,7 @@ public class Unit : NetworkBehaviour, IAttackable
         yield return new WaitForSeconds(time);
         if (target != null)
             target.GetComponent<IAttackable>().ReceiveDamage(atkDmg);
+        yield return null;
     }
 
     private void ChangeAnimation(string name)
@@ -114,6 +129,17 @@ public class Unit : NetworkBehaviour, IAttackable
             GetComponent<Animator>().SetBool(name, true);
         else
             GetComponent<Animation>().Play(name);
+    }
+
+    private void ChangeAnimation(string name, float animeSpeed = 1.0f)
+    {
+        if (hasAnimator)
+            GetComponent<Animator>().SetBool(name, true);
+        else
+        {
+            GetComponent<Animation>()[name].speed = GetComponent<Animation>()[name].length / animeSpeed;
+            GetComponent<Animation>().Play(name, PlayMode.StopAll);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -142,6 +168,8 @@ public class Unit : NetworkBehaviour, IAttackable
         currentLife -= damage;
         if (currentLife <= 0)
             isDead = true;
+        else
+        healthBar.fillAmount = (float)currentLife / (float)maxLife;
     }
 
     public GameObject GetGameObject()
