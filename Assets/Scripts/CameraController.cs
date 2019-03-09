64a235f4db760b6ba7cfd65225bbded797857b12
David Maidenberg <david.maidenberg@epitech.eu>
2019-03-08 18:58:40 -0800

    Add CameraController class, and change the angle of the carmera and healthbar

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 50f;
+
+    void Update()
+    {
+        Vector3 updatedPosition = Vector3.zero;
+
+        if (Input.mousePosition.y < 10f)
+            updatedPosition -= Vector3.right * cameraSpeed * Time.deltaTime;
+        else if (Input.mousePosition.y > Screen.height - 10f)
+            updatedPosition += Vector3.right * cameraSpeed * Time.deltaTime;
+        if (Input.mousePosition.x < 10f)
+            updatedPosition += Vector3.forward * cameraSpeed * Time.deltaTime;
+        else if (Input.mousePosition.x > Screen.width - 10f)
+            updatedPosition -= Vector3.forward * cameraSpeed * Time.deltaTime;
+
+        this.transform.position += updatedPosition;
+    }
+}
--- Assets/Scripts/Nexus.cs
+++ Assets/Scripts/Nexus.cs
@@ -7,12 +7,8 @@ public class Nexus : Tower
 {
     public int pPidOwner;
     public Image healthBar;
-    public GameObject healthBarGO;
 
     protected override void Start()
     {
-        base.Start();
-        if (pPidOwner == 1)
-            healthBarGO.transform.Rotate(new Vector3(90f, -180f, 0f));
     }
 }
