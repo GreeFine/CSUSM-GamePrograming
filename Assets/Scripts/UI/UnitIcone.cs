bbe524b63e52e7fb25fcc29eaa441386abaad4a3
David Maidenberg <david.maidenberg@epitech.eu>
2019-05-02 11:38:12 -0700

    Basic level design

---
+++ Assets/Scripts/UI/UnitIcone.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitIcone : MonoBehaviour
+{
+    public Sprite icone;
+    public string manaCost;
+    public string unit;
+
+    private void Start()
+    {
+        GetComponent<Image>().sprite = icone;
+        GetComponentInChildren<Text>().text = manaCost;
+    }
+
+    private void OnMouseOver()
+    {
+        
+    }
+
+    public void OnIconeClick()
+    {
+       
+    }
+}
